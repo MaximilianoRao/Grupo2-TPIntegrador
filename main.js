67c836d7cc09c7b81f022b64e755d735d5e51244
MaximilianoRao <maxism96@gmail.com>
2023-04-04 03:58:07 -0300

    Se agrega funcionalidad a boton anterior

--- main.js
+++ main.js
@@ -17,12 +17,13 @@ $(document).ready(function() {
     });
 
 
-    $('.nbtnext').click(function () {
+    $('.nbtnext, .nbtend').click(function () {
       // Cambiar el nav-link activo al siguiente
       //$('#paso1-tab').next().find('a').tab('show');
       let id = $(this).parent().attr('id');
-      let paso = parseInt($('.card-header-tabs .nav-item').find(".active").attr('href')[5]);
-      let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
+      id = '#'+id;
+      let paso = parseInt(id[5]);
+      //let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
       let nuevaposicion = '#step'+ (paso+1);
       nuevaPestaña = $('a.nav-link[href$="'+ (paso+1) +'"]');
       pestañaActual = $('.card-header-tabs .nav-item').find(".active")
@@ -30,7 +31,7 @@ $(document).ready(function() {
       pestañaActual[0].tabIndex = -1;
       nuevaPestaña[0].ariaSelected = true;
       nuevaPestaña[0].tabIndex = 0;
-      $(posicion).removeClass('active show')
+      $(id).removeClass('active show')
       $(nuevaposicion).addClass('active show')
        pestañaActual.removeClass('active');
        nuevaPestaña.removeClass('disabled');
@@ -40,8 +41,26 @@ $(document).ready(function() {
       //$('.card-header-tabs .nav-item').find(".active").removeClass('active');
     });
 
+    $('.nbtprevius').click(function () {
+      let id = $(this).parent().attr('id');
+      id = '#'+id;
+      let paso = parseInt(id[5]);
+      //let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
+      let nuevaposicion = '#step'+ (paso-1);
+      nuevaPestaña = $('a.nav-link[href$="'+ (paso-1) +'"]');
+      pestañaActual = $('.card-header-tabs .nav-item').find(".active")
+      pestañaActual[0].ariaSelected = false;
+      pestañaActual[0].tabIndex = -1;
+      nuevaPestaña[0].ariaSelected = true;
+      nuevaPestaña[0].tabIndex = 0;
+      $(id).removeClass('active show')
+      $(nuevaposicion).addClass('active show')
+       pestañaActual.removeClass('active');
+       nuevaPestaña.removeClass('disabled');
+       nuevaPestaña.addClass('active');
+    });
 
-    
+
 
 
 
