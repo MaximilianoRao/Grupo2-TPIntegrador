18b9925f48c10495d288ac8edb15fa478ecd0313
MaximilianoRao <maxism96@gmail.com>
2023-04-04 02:58:42 -0300

    Se agrega funcionalidad al boton siguiente

--- main.js
+++ main.js
@@ -20,16 +20,28 @@ $(document).ready(function() {
     $('.nbtnext').click(function () {
       // Cambiar el nav-link activo al siguiente
       //$('#paso1-tab').next().find('a').tab('show');
-     // console.log($(this).parent().attr('id'));
       let id = $(this).parent().attr('id');
-      let posicion = parseInt($('.card-header-tabs .nav-item').find(".active").attr('href')[5]);
-       // $('.card-header-tabs .nav-item').find(".active").addClass('disabled');
-       // $('.card-header-tabs .nav-item').find(".active").removeClass('active');
-       
+      let paso = parseInt($('.card-header-tabs .nav-item').find(".active").attr('href')[5]);
+      let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
+      let nuevaposicion = '#step'+ (paso+1);
+      nuevaPestaña = $('a.nav-link[href$="'+ (paso+1) +'"]');
+      pestañaActual = $('.card-header-tabs .nav-item').find(".active")
+      pestañaActual[0].ariaSelected = false;
+      pestañaActual[0].tabIndex = -1;
+      nuevaPestaña[0].ariaSelected = true;
+      nuevaPestaña[0].tabIndex = 0;
+      $(posicion).removeClass('active show')
+      $(nuevaposicion).addClass('active show')
+       pestañaActual.removeClass('active');
+       nuevaPestaña.removeClass('disabled');
+       nuevaPestaña.addClass('active');
         //parseInt($('.card-header-tabs .nav-item').find(".disabled").[0].attributes.href.textContent[5]);
       
       //$('.card-header-tabs .nav-item').find(".active").removeClass('active');
     });
+
+
+    
 
 
 
