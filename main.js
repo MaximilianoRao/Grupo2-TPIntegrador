ba2132ebdf8dcfaf5cd9675fb3b975b44073a45f
MaximilianoRao <maxism96@gmail.com>
2023-04-12 17:16:28 -0300

    Validación form

--- main.js
+++ main.js
@@ -162,11 +162,9 @@ $(document).ready(function() {
       };
     
       // Agrega la validación al formulario utilizando jQuery
-      $('.nbtnext, .nbtend').click(function (){
+      function validacion(id){
         
-        let id = $(this).parent().attr('id');
-        id = '#'+id;
-        console.log(id);
+       
         let paso = parseInt(id[5]);
         //let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
         let nuevaposicion = '#step'+ (paso+1);
@@ -236,20 +234,77 @@ $(document).ready(function() {
                 }
 
             });
-          }      
+          }  
+          return false;    
         } else {
-          pestañaActual[0].ariaSelected = false;
-          pestañaActual[0].tabIndex = -1;
-          nuevaPestaña[0].ariaSelected = true;
-          nuevaPestaña[0].tabIndex = 0;
-          $(id).removeClass('active show')
-          $(nuevaposicion).addClass('active show')
-           pestañaActual.removeClass('active');
-           nuevaPestaña.removeClass('disabled');
-           nuevaPestaña.addClass('active');
+          return true;
+        }
+      }
+
+      $('.nbtnext, .nbtend').click(function () {
+        // Cambiar el nav-link activo al siguiente
+        //$('#paso1-tab').next().find('a').tab('show');
+        let id = $(this).parent().attr('id');
+        id = '#'+id;
+        if(validacion(id)){
+        let paso = parseInt(id[5]);
+        //let posicion = $('.card-header-tabs .nav-item').find(".active").attr('href');
+        let nuevaposicion = '#step'+ (paso+1);
+        nuevaPestaña = $('a.nav-link[href$="'+ (paso+1) +'"]');
+        pestañaActual = $('.card-header-tabs .nav-item').find(".active")
+        
+        pestañaActual[0].ariaSelected = false;
+        pestañaActual[0].tabIndex = -1;
+        nuevaPestaña[0].ariaSelected = true;
+        nuevaPestaña[0].tabIndex = 0;
+        $(id).removeClass('active show')
+        $(nuevaposicion).addClass('active show')
+         pestañaActual.removeClass('active');
+         nuevaPestaña.removeClass('disabled');
+         nuevaPestaña.addClass('active');
+         }
+      }); 
+      
+      
+      
+       $('.cotizador a.nav-link').on('click', function(e) {
+        // Prevenir que se active la pestaña si los campos no son válidos
+        let id = $(this).attr('href');
+        let link = $(this);
+        console.log(id);
+        
+        let $campos1 = $(id).find('input, textarea');
+        let $campos2 = $(id).find('select');
+
+        
+        $($campos1).on('keyup', function() {
+            if (!(validacion(id))) {
+              $('.cotizador a.nav-link').each(function() {
+                let href = $(this).attr('href');
+                if (href > id) {
+                  $(this).addClass('disabled');
+                }
+              });
+          }
+        });
+
+        $($campos2).on('change', function() {
+          if (!(validacion(id))) {
+            $('.cotizador a.nav-link').each(function() {
+              let href = $(this).attr('href');
+              if (href > id) {
+                $(this).addClass('disabled');
+              }
+            });
         }
       });
+        
+        
+      });
+
     });
+
+    
 
     $('.nbtprevius').click(function () {
       let id = $(this).parent().attr('id');
